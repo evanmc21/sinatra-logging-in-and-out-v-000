ea0556c4c9879804796cc1435f87198eb2e5db45
Evan Candler <emcandler21@gmail.com>
2018-04-16 15:44:31 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(:username params["username"])
+    redirect '/users/account'
   end
 
   get '/account' do
